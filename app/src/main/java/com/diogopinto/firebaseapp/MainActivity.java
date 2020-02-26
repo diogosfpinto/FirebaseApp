35b6e9484de6edaf782bd194ac711bb9c44d9803
Diogo S F Pinto <diogo.sfpinto@gmail.com>
2020-02-25 22:20:47 -0400

    retornando dados do firebase

--- app/src/main/java/com/diogopinto/firebaseapp/MainActivity.java
+++ app/src/main/java/com/diogopinto/firebaseapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.diogopinto.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.usage.NetworkStatsManager;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,23 +25,38 @@ public class MainActivity extends AppCompatActivity {
 //        reference.child("usuarios2").child("001").child("nome").setValue("Emanoel");
 
        //utilizando objetos
-       /*DatabaseReference usuarios = reference.child("usuarios");
+       DatabaseReference usuarios = reference.child("usuarios");
+       
+       usuarios.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               Log.i("FIREBASE", dataSnapshot.getValue().toString());
+           }
 
-       Usuarios usuario = new Usuarios();
+           @Override
+           public void onCancelled(@NonNull DatabaseError databaseError) {
+
+           }
+       });
+
+
+//      Salvando em banco de dados
+       /*Usuarios usuario = new Usuarios();
 
        usuario.setNome("Marilia");
        usuario.setSobrenome("Mendonca");
        usuario.setIdade(28);
 
+
        usuarios.child("002").setValue(usuario);
 
-       */
+
        DatabaseReference produtos = reference.child("produtos");
        Produtos produto = new Produtos();
        produto.setDescricao("Notebook");
        produto.setMarca("Samsung");
        produto.setPreco(2000.89);
 
-       produtos.child("002").setValue(produto);
+       produtos.child("002").setValue(produto);*/
     }
 }
